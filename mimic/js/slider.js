4df4e3e720e1d6542182492353fde3acb56e6038
psaux0 <russell.susu@gmail.com>
2015-11-18 22:27:19 -0500

    uber radio button

---
+++ mimic/js/slider.js
@@ -0,0 +1,38 @@
+function getScrollBar(){
+  return document.getElementById('scrollBarHider');
+}
+
+var initalizeScrollBar = function() {
+  getScrollBar().scrollLeft = 0;
+}
+
+initalizeScrollBar();
+
+var run = function(){
+//  var scrollController = document.getElementById('scrollBarHider');
+
+  scrollController = getScrollBar();
+  var scrollX = scrollController.scrollLeft;
+  //document.getElementById('logger').innerHTML = 'scrollX: ' + scrollX;
+  var scLength = scrollController.offsetWidth;
+  var ctLength = document.getElementById('content').offsetWidth;
+
+  var sL = ctLength - scLength;
+  var p1 = document.getElementById('p1');
+  var p2 = document.getElementById('p2');
+  var p3 = document.getElementById('p3');
+
+  if(scrollX <= sL/3) {
+    p1.checked = true;
+    p2.checked = false;
+    p3.checked = false;
+  } else if(scrollX > sL/3 && scrollX <= 2 * sL/3) {
+    p2.checked = true;
+    p3.checked = false;
+    p1.checked = false;
+  } else {
+    p3.checked = true;
+    p2.checked = false;
+    p1.checked = false;
+  }
+}
