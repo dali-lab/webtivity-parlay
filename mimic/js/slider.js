a41bead7f70af8d8e377b22bd12b5f2f303b6d11
psaux0 <russell.susu@gmail.com>
2015-11-19 08:56:55 -0500

    add status bar

--- mimic/js/slider.js
+++ mimic/js/slider.js
@@ -21,18 +21,22 @@ var run = function(){
   var p1 = document.getElementById('p1');
   var p2 = document.getElementById('p2');
   var p3 = document.getElementById('p3');
+  var mb = document.getElementById('mb');
 
   if(scrollX <= sL/3) {
     p1.checked = true;
     p2.checked = false;
     p3.checked = false;
+    mb.style.width = '0';
   } else if(scrollX > sL/3 && scrollX <= 2 * sL/3) {
     p2.checked = true;
     p3.checked = false;
     p1.checked = false;
+    mb.style.width = '50%';
   } else {
     p3.checked = true;
     p2.checked = false;
     p1.checked = false;
+    mb.style.width = '100%';
   }
 }
