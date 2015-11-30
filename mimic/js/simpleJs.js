c0543c63f2903c1512bc117b1ccd7c5a5f9d9acb
psaux0 <russell.susu@gmail.com>
2015-11-30 08:59:23 -0500

    resize font and slider

--- mimic/js/simpleJs.js
+++ mimic/js/simpleJs.js
@@ -1,3 +1,19 @@
 var rangeObject = document.getElementById('rangeObj');
 
 //rangeObject.value = 0;
+
+var vv = rangeObject.value;
+var s = rangeObject.step;
+
+rangeObject.addEventListener('input', function(){
+  if(rangeObject.value > vv) {
+    for(var i = 0; i < 30/s; i++) {
+      this.stepUp(s);
+    }
+  }
+  if(rangeObject.value < vv) {
+    for(var i = 0; i < 30/s; i++) {
+      this.stepDown(s);
+    }
+  }
+});
